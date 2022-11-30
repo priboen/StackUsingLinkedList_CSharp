2a92995aba1ac827560b7d54a7703fcf592f0da9
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-11-30 15:36:49 +0700

    create Stack class

--- Program.cs
+++ Program.cs
@@ -16,6 +16,10 @@ namespace StackUsingLinkedList_CSharp
             next = n;
         }
     }
+    class Stack
+    {
+
+    }
     internal class Program
     {
         static void Main(string[] args)
