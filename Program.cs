ce9ed5b5ad5f06472ed6bac605d6da045a5c141b
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-11-30 15:38:37 +0700

    create empty method type boolean

--- Program.cs
+++ Program.cs
@@ -23,6 +23,13 @@ namespace StackUsingLinkedList_CSharp
         {
             top = null;
         }
+        bool empty()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
     }
     internal class Program
     {
