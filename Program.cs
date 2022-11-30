eb2e897f74dda8b4ec517cfdac46bac2a4d51dbe
Muhamad Adri Muwaffaq Khamid <muhamad.adri.ft21@mail.umy.ac.id>
2022-11-30 16:08:03 +0700

    add case function in switch method

--- Program.cs
+++ Program.cs
@@ -65,6 +65,7 @@ namespace StackUsingLinkedList_CSharp
             Stack s = new Stack();
             while (true)
             {
+                Console.Clear();
                 Console.WriteLine();
                 Console.WriteLine("\n ***Stack Menu***\n");
                 Console.WriteLine("1. Push");
@@ -76,7 +77,36 @@ namespace StackUsingLinkedList_CSharp
                 char ch = Convert.ToChar(input == "" ? "0" : input);
                 switch (ch)
                 {
+            
+                    case '1':
+                        Console.Clear();
+                        Console.Write("\n Enter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
 
+                    case '2':
+                        Console.Clear();
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+
+                    case '3':
+                        Console.Clear();
+                        s.display();
+                        break;
+
+                    case '4':
+                        Console.Clear();
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine("\n Invalid Choice");
+                        break;
                 }
             }
         } 
